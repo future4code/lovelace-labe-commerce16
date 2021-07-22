b7adc66b18193276917308c6f607cae96135adba
GuilhermeAAmaral <gui_lhermeaugusto@live.com>
2021-07-22 13:31:03 -0300

    filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,17 +1,49 @@
 import React from "react";
-import { SmallContainer } from "../App.styles";
+import { CardContainer, SmallContainer } from "../App.styles";
 
 export default class Filters extends React.Component {
+
+  state = {
+
+    filtro: [
+      { 
+        id: Date.now(),
+        valorMinimo: 10,
+        valorMaximo: 100,
+        buscarProduto: ''
+      }
+    ],
+
+    inputValorMinimo: '',
+    inputValorMaximo: '',
+    inputBuscarProdutos: ''
+
+  }
+
+  onChangeInputValorMinimo = (event) => {
+    this.setState ({inputValorMinimo: event.target.value})
+  }
+
+  onChangeInputValorMaximo = (event) => {
+    this.setState ({inputValorMaximo: event.target.value})
+  }
+
+  onChangeInputBuscarProdutos = (event) => {
+    this.setState ({inputBuscarProdutos: event.target.value})
+  }
+
+
   render() {
+
     return (
       <SmallContainer>
         <h3>Filtros</h3>
         <p>Valor mínimo:</p>
-        <input type="number" value="100" />
+        <input value={this.state.inputValorMinimo} onChange={this.onChangeInputValorMinimo} type="number"/>
         <p>Valor máximo:</p>
-        <input type="number" value="100" />
+        <input value={this.state.inputValorMaximo} onChange={this.onChangeInputValorMaximo}type="number"/>
         <p>Buscar por nome:</p>
-        <input type="text" value="Produto 1" />
+        <input value ={this.state.inputBuscarProdutos} onChange={this.onChangeInputBuscarProdutos} type="text"/>
       </SmallContainer>
     );
   }
