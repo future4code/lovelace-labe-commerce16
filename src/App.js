32dccc131b78379e6fb75a40d4b0117d5aef59ba
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-07-22 11:13:04 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-import React from "react";
-import { GlobalStyle } from "./App.styles";
-import { AppContainer } from "./App.styles";
-import Filters from "./components/Filters";
-import Products from "./components/Products";
-import ShoppingCart from "./components/ShoppingCart";
+import logo from './logo.svg';
+import './App.css';
 
-export default class App extends React.Component {
-  state = {
-    produtos: [
-      {
-        id: 1,
-        nome: "Produto 1",
-        preco: 100,
-        imagem: "https://picsum.photos/200",
-      },
-      {
-        id: 2,
-        nome: "Produto 2",
-        preco: 100,
-        imagem: "https://picsum.photos/200",
-      },
-      {
-        id: 3,
-        nome: "Produto 3",
-        preco: 100,
-        imagem: "https://picsum.photos/200",
-      },
-    ],
-  };
-
-  render() {
-    return (
-      <AppContainer>
-        <GlobalStyle />
-        <Filters />
-        <Products />
-        <ShoppingCart />
-      </AppContainer>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
