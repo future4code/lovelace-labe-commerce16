ee19969db18ec612443bdd4c3698932a29c287d4
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-07-22 11:20:02 -0300

    Arquivos Estáticos criados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GlobalStyle } from "./App.styles";
+import { AppContainer } from "./App.styles";
+import Filters from "./components/Filters";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    produtos: [
+      {
+        id: 1,
+        nome: "Produto 1",
+        preco: 100,
+        imagem: "https://picsum.photos/200",
+      },
+      {
+        id: 2,
+        nome: "Produto 2",
+        preco: 100,
+        imagem: "https://picsum.photos/200",
+      },
+      {
+        id: 3,
+        nome: "Produto 3",
+        preco: 100,
+        imagem: "https://picsum.photos/200",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <AppContainer>
+        <GlobalStyle />
+        <Filters />
+        <Products />
+        <ShoppingCart />
+      </AppContainer>
+    );
+  }
 }
-
-export default App;
